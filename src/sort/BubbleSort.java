bcb76883f4bc5eb128f44a5c02ea170e57b35260
pengshiquan <1162210866@qq.com>
2021-08-15 23:02:31 +0800

    增加了冒泡排序的优化方法三
    增加了冒泡排序的优化方法三

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -10,13 +10,20 @@ package sort;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = new int[]{0, 2, 7, 3, 4, 5, 6, 1, 2};
-        bubbleSort2(arr);
+        bubbleSort3(arr);
         for (int i : arr) {
             System.out.print(i);
         }
-
     }
 
+    /**
+     * Description: 排序算法基础版
+     *
+     * @param arr
+     * @return void
+     * @Author Peng Shiquan
+     * @Date 2021-08-15
+     */
     public static void bubbleSort1(int[] arr) {
         int length = arr.length;
         for (int i = 0; i < length - 1; i++) {
@@ -26,12 +33,21 @@ public class BubbleSort {
         }
     }
 
+    /**
+     * Description: 优化了排序的条件判断
+     *
+     * @param arr
+     * @return void
+     * @Author Peng Shiquan
+     * @Date 2021-08-15
+     */
     public static void bubbleSort2(int[] arr) {
         boolean swaped = true;
         int length = arr.length;
         for (int i = 0; i < length - 1; i++) {
             //如果没有发生交换，代表当前数组有序，直接返回即可
             if (!swaped) break;
+            swaped = false;
             for (int j = 0; j < length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     BaseSort.swap(arr, j, j + 1);
@@ -41,5 +57,38 @@ public class BubbleSort {
         }
     }
 
-
+    /**
+     * Description: 记录排序的位置
+     *
+     * @param arr
+     * @return void
+     * @Author Peng Shiquan
+     * @Date 2021-08-15
+     */
+    public static void bubbleSort3(int[] arr) {
+        /**
+         *  当前循环是否交换
+         */
+        boolean swaped = true;
+        /**
+         * 当前循环的最后一个index
+         */
+        int lastIndex = arr.length - 1;
+        /**
+         * 最后交换的数组下标
+         */
+        int swapedIndex = -1;
+        while (swaped) {
+            swaped = false;
+            //因为第一次的循环，交换的index只会到arr.length - 2,所以这里用小于
+            for (int i = 0; i < lastIndex; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    BaseSort.swap(arr, i, i + 1);
+                    swaped = true;
+                    swapedIndex = i;
+                }
+            }
+            lastIndex = swapedIndex;
+        }
+    }
 }
