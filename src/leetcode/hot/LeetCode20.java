b4e95e6b0f272dd5deddb2c433a68eca0fb1a06f
pengshiquan <1162210866@qq.com>
2022-08-11 15:47:35 +0800

    完成有效的括号题目
    完成有效的括号题目

--- src/leetcode/hot/LeetCode20.java
+++ src/leetcode/hot/LeetCode20.java
@@ -1,20 +1,49 @@
 package leetcode.hot;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * LeetCode20.java
- * Description:
+ * Description: https://leetcode.cn/problems/valid-parentheses/
  *
  * @author Peng Shiquan
  * @date 2022/8/9
  */
 public class LeetCode20 {
     public static void main(String[] args) {
-        new LeetCode20().isValid("()[]{}");
-
+        System.err.println(new LeetCode20().isValid("()[]{"));
     }
 
     public boolean isValid(String s) {
-        return false;
+        char[] chars = s.toCharArray();
+        if (chars.length % 2 == 1) return false;
+        Deque<Character> st = new LinkedList<>();
+        for (char b : chars) {
+            switch (b) {
+                case '(':
+                case '[':
+                case '{':
+                    st.push(b);
+                    break;
+                case ')':
+                    if (st.isEmpty() || !st.peek().equals('(')) return false;
+                    st.pop();
+                    break;
+                case ']':
+                    if (st.isEmpty() || !st.peek().equals('[')) return false;
+                    st.pop();
+                    break;
+                case '}':
+                    if (st.isEmpty() || !st.peek().equals('{')) return false;
+                    st.pop();
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return st.isEmpty();
     }
 
 
